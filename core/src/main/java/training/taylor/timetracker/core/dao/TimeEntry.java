97e131703369ff1b0850271e872311c2dee517a5
roberuto76 <71687055+roberuto76@users.noreply.github.com>
2020-09-23 15:00:25 +0900

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -13,21 +13,25 @@ public class TimeEntry {
     private float rate;
     private int time;
 
-public float getRate() {
-    return rate;
-}
+    public void setDescription(String value) {
+        this.description = value;
+    }
+    
+    public float getRate() {
+        return rate;
+    }
 
-public void setRate(float value) {
-    this.rate = value;
-}
+    public void setRate(float value) {
+        this.rate = value;
+    }
 
-public int getTime() {
-    return time;
-}
+    public int getTime() {
+        return time;
+    }
 
-public void setTime(int value) {
-    this.time = value;
-}
+    public void setTime(int value) {
+        this.time = value;
+    }
     
     @Override
     public String toString() {
